60a9a0d0b769fa70fa2e1b2185b1a712185cc2bb
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2023-09-26 13:26:28 +0100

    updated footer with link to github

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      <br />
+      <footer>
+        <a
+          href="https://github.com/VSeaman8/react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open Sourced
+        </a>
+        by Vicki Seaman
+      </footer>
     </div>
   );
 }
